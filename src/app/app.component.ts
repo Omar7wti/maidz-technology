07ac8934e8f1ebdbd89421121069ed2e820a05c7
omar adel <elhwety5555@gmail.com>
2024-05-13 16:07:47 +0300

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { UsersComponent } from './users/users.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [CommonModule, RouterOutlet, HeaderComponent, UsersComponent],
 })
 export class AppComponent {
   title = 'maidz-dashboard';
